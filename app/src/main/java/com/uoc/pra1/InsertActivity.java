89bf14769370be5d0edd56894c0a7c089d04dd66
Carlos Villar Deyá <carlos.villar.deya@gmail.com>
2017-12-03 14:46:54 +0100

    1.4

--- app/src/main/java/com/uoc/pra1/InsertActivity.java
+++ app/src/main/java/com/uoc/pra1/InsertActivity.java
@@ -1,15 +1,20 @@
 package com.uoc.pra1;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.drawable.BitmapDrawable;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
@@ -37,15 +42,22 @@ public class InsertActivity extends AppCompatActivity {
     ImageView image;
     TextView txtLatitudeLongitude;
 
+    private static final int PETICION_PERMISO_LOCALIZACION = 101;
+
+    public LocationManager locationManager;
+    public LocationListener locationListener;
 
     private static double longitude;
     private static double latitude;
 
-    LocationManager locationManager;
-
     private Location location;
 
     private String[] permissions;
+
+
+
+
+    private static final String ERR_PERMISSION = "Permission denied. Check permissions on AndroidManifest.xml.";
 
     private int REQUEST_CAMERA = 0, SELECT_FILE = 1;
 
@@ -57,29 +69,81 @@ public class InsertActivity extends AppCompatActivity {
         setContentView(R.layout.activity_insert);
         mContext=this;
 
+
         // Cambiamos el título de la actividad
         android.support.v7.app.ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("PR2 :: Insert");
 
         // Location
-        locationManager=(LocationManager) mContext.getSystemService(Context.LOCATION_SERVICE);
-       // locationManager.requestLocationUpdates( LocationManager.GPS_PROVIDER, 2000, 10, locationListener);
-
-
         // Presentamos los datos de Longitud y Latitud
         txtLatitudeLongitude=(TextView)this.findViewById(R.id.textViewLatitudeLongitude);
 
+        locationManager=(LocationManager) mContext.getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates( LocationManager.GPS_PROVIDER, 2000, 10, locationListenerGPS);
+       // isLocationEnabled();
 
-       // Location location;
+        location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if(location!=null){
+            // set TextView
+            latitude=location.getLatitude();
+            longitude=location.getLongitude();
 
-        // double latitude = location.getLatitude();
-        // double longitude = location.getLongitude();
-        //String msg="New Latitude: "+latitude + "New Longitude: "+longitude;
-        //Toast.makeText(mContext,msg,Toast.LENGTH_LONG).show();
-        //txtLatitude.setText("Latitude: " + latitude);
-        //txtLongitude.setText("Longitude: " + longitude);
+           // String msg = "New Latitude: "+String.valueOf(location.getLatitude())+"New Longitude: "+String.valueOf(location.getLongitude());
+           // Toast.makeText(getBaseContext(), msg, Toast.LENGTH_LONG).show();
+            txtLatitudeLongitude.setText("lat:"+latitude+" lon:"+longitude);
+        }else{
+            String msg = "Ha fallado la obtención de los datos de posición";
+            Toast.makeText(getBaseContext(), msg, Toast.LENGTH_LONG).show();
+        }
+        //locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIEMPO_ENTRE_UPDATES, MIN_CAMBIO_DISTANCIA_PARA_UPDATES, locationListener);
 
 
+ /*       locationListener = new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+
+                    if(location != null) {
+                        txtLatitudeLongitude.setText("Latitude="+location.getLatitude()+" " + "Longitude="+location.getLongitude());
+                    };
+
+                    if (ContextCompat.checkSelfPermission(InsertActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        // Ask permission
+                        ActivityCompat.requestPermissions(InsertActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PETICION_PERMISO_LOCALIZACION);
+                    } else {
+                        // Ask last know location
+                        location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                         if(location!=null){
+                            // set TextView
+                            latitude=location.getLatitude();
+                            longitude=location.getLongitude();
+                           // String msg = "New Latitude: "+location.getLatitude()+"New Longitude: "+location.getLongitude();
+                            String msg = "New Latitude: "+String.valueOf(location.getLatitude())+"New Longitude: "+String.valueOf(location.getLongitude());
+                            Toast.makeText(getBaseContext(), msg, Toast.LENGTH_LONG).show();
+                            txtLatitudeLongitude.setText("lat:"+latitude+" lon:"+longitude);
+                        } else {
+                            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+                        }
+                    }
+
+                }
+
+                @Override
+                public void onStatusChanged(String s, int i, Bundle bundle) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String s) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String s) {
+
+                }
+
+            };
+*/
         // Definimos la imagen, el botón para cambiar la imagen y el listener asociado
         image = (ImageView) findViewById(imageView);
 
@@ -178,7 +242,21 @@ public class InsertActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        try {
+            switch (requestCode) {
+                case 101: {
+                    if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+                    } else {
+                    }
+                    return;
+                }
+            }
+        } catch(SecurityException ERR_PERMISSION) { }
 
+    }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
@@ -187,32 +265,17 @@ public class InsertActivity extends AppCompatActivity {
         return true;
     }
 
-    LocationListener locationListener = new LocationListener() {
-        @Override
-        public void onLocationChanged(android.location.Location location) {
-            double latitude = location.getLatitude();
-            double longitude = location.getLongitude();
-            //String msg="New Latitude: "+latitude + "New Longitude: "+longitude;
-            //Toast.makeText(mContext,msg,Toast.LENGTH_LONG).show();
-            //txtLatitude.setText("Latitude: " + latitude);
-            //txtLongitude.setText("Longitude: " + longitude);
-        }
+    private void galleryIntent() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select File"),SELECT_FILE);
+    }
 
-        @Override
-        public void onStatusChanged(String provider, int status, Bundle extras) {
-
-        }
-
-        @Override
-        public void onProviderEnabled(String provider) {
-
-        }
-
-        @Override
-        public void onProviderDisabled(String provider) {
-
-        }
-    };
+    private void cameraIntent() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, REQUEST_CAMERA);
+    }
 
     private void selectImage() {
         final CharSequence[] items = { "Take Photo", "Choose from Library", "Cancel" };
@@ -242,17 +305,72 @@ public class InsertActivity extends AppCompatActivity {
         builder.show();
     }
 
-    private void galleryIntent() {
-        Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);//
-        startActivityForResult(Intent.createChooser(intent, "Select File"),SELECT_FILE);
+    LocationListener locationListenerGPS=new LocationListener() {
+        @Override
+        public void onLocationChanged(android.location.Location location) {
+            double latitude=location.getLatitude();
+            double longitude=location.getLongitude();
+            String msg="New Latitude: "+latitude + "New Longitude: "+longitude;
+            Toast.makeText(mContext,msg,Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+
+
+    private void isLocationEnabled() {
+
+        if(!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+     /*       AlertDialog.Builder alertDialog=new AlertDialog.Builder(mContext);
+            alertDialog.setTitle("Enable Location");
+            alertDialog.setMessage("Your locations setting is not enabled. Please enabled it in settings menu.");
+            alertDialog.setPositiveButton("Location Settings", new DialogInterface.OnClickListener(){
+                public void onClick(DialogInterface dialog, int which){
+                    Intent intent=new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    startActivity(intent);
+                }
+            });
+            alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener(){
+                public void onClick(DialogInterface dialog, int which){
+                    dialog.cancel();
+                }
+            });
+            AlertDialog alert=alertDialog.create();
+            alert.show();
+      */  }
+        else{
+            AlertDialog.Builder alertDialog=new AlertDialog.Builder(mContext);
+            alertDialog.setTitle("Confirm Location");
+            alertDialog.setMessage("Your Location is enabled, please enjoy");
+            alertDialog.setNegativeButton("Back to interface",new DialogInterface.OnClickListener(){
+                public void onClick(DialogInterface dialog, int which){
+                    dialog.cancel();
+                }
+            });
+            AlertDialog alert=alertDialog.create();
+            alert.show();
+        }
     }
 
-    private void cameraIntent() {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(intent, REQUEST_CAMERA);
+ /*   protected void onResume(){
+        super.onResume();
+        isLocationEnabled();
     }
+*/
+
 
 }
 
